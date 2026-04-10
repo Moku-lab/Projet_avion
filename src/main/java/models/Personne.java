a9a2fb2e2b500dffcf27b8b79e2e13dde0e53863
jadorefinalement <emericdb0@gmail.com>
2026-04-10 17:01:48 +0200

    test

--- src/main/java/models/Personne.java
+++ src/main/java/models/Personne.java
@@ -1,4 +1,19 @@
 package models;
 
-public class Personne {
+public abstract class Personne {
+    protected String identifiant;
+    protected String nom;
+    protected String adresse;
+    protected String contact;
+
+    public Personne(String identifiant, String nom, String adresse, String contact) {
+        this.identifiant = identifiant;
+        this.nom = nom;
+        this.adresse = adresse;
+        this.contact = contact;
+    }
+
+    public void obtenirInfos() {
+        System.out.println("ID: " + identifiant + " | Nom: " + nom);
+    }
 }
