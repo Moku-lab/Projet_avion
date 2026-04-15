cf11aa5c399026fae2a947d0302cd0f0399932e6
Moku-lab <blinenlai@gmail.com>
2026-04-15 12:54:57 +0200

    class Aeroport ajoutée

--- src/main/java/models/Aeroport.java
+++ src/main/java/models/Aeroport.java
@@ -1,4 +1,22 @@
 package models;
 
 public class Aeroport {
+    private String nom;
+    private String ville;
+    private String description;
+
+    public Aeroport (String nom, String ville, String description){
+        this.nom = nom;
+        this.ville = ville;
+        this.description = description;
+    }
+
+    public String getNom(){
+        return nom;
+    }
+
+    public void assignFlight(){
+        System.out.println("Vol affecté à l'aéroport " + nom);
+    }
+
 }
